3698adee458e01017ed4d47d67a0ddb895ade963
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-02-11 16:10:28 +0630

    chore: change socket event name format

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,9 +1,11 @@
-const CREATE_ROOM = 'create room'
-const JOIN_ROOM = 'join room'
-const ROOM_NOT_FOUND = 'room not found'
+const CREATE_ROOM = 'create-room'
+const JOIN_ROOM = 'join-room'
+const ROOM_NOT_FOUND = 'room-not-found'
+const ROOM_JOINED = 'room-joined'
 
 export {
   CREATE_ROOM,
   JOIN_ROOM,
   ROOM_NOT_FOUND,
+  ROOM_JOINED,
 }
---
+++ server/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  CREATE_ROOM: 'create-room',
+  JOIN_ROOM: 'join-room',
+  ROOM_NOT_FOUND: 'room-not-found',
+  ROOM_JOINED: 'room-joined',
+}
