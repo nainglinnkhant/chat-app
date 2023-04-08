36a8226b659b9d70f9ef9a37e69cc9efc2ef67c8
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-04-09 01:10:52 +0630

    feat: add emoji picker in chat message input

--- client/src/components/Chat/MessageInput.tsx
+++ client/src/components/Chat/MessageInput.tsx
@@ -1,7 +1,11 @@
 import { FormEvent, useState } from 'react'
+import data from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
 
 import { SEND_MESSAGE } from '../../constants/eventNames'
 import { socket } from '../../socket'
+import EmojiIcon from '../Icons/EmojiIcon'
+import ImageIcon from '../Icons/ImageIcon'
 import styles from '../../pages/Room.module.css'
 
 interface MessageInputProps {
@@ -11,15 +15,46 @@ interface MessageInputProps {
 
 const MessageInput = ({ roomName, userId }: MessageInputProps) => {
   const [message, setMessage] = useState('')
+  const [isPickerOpen, setIsPickerOpen] = useState(false)
 
   const sendMessage = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     socket.emit(SEND_MESSAGE, { roomName, senderId: userId, message })
     setMessage('')
+    setIsPickerOpen(false)
   }
 
   return (
     <form className={styles['message-form']} onSubmit={sendMessage}>
+      {isPickerOpen && (
+        <div className={styles['emoji-picker']}>
+          <div
+            className={styles['picker-overlay']}
+            onClick={() => setIsPickerOpen(false)}
+          />
+
+          <Picker
+            data={data}
+            theme='dark'
+            onEmojiSelect={(data: { native: string }) =>
+              setMessage(prevMessage => `${prevMessage}${data.native}`)
+            }
+          />
+        </div>
+      )}
+
+      <button
+        type='button'
+        onClick={() => setIsPickerOpen(prevState => !prevState)}
+        style={{ marginRight: 12 }}
+      >
+        <EmojiIcon color='#979595' />
+      </button>
+
+      <button type='button'>
+        <ImageIcon color='#979595' />
+      </button>
+
       <input
         value={message}
         onChange={e => setMessage(e.target.value)}
