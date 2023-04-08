f23a9ddf9bb6d6753ee683dc6bc8f02e16783572
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-04-09 01:07:34 +0630

    fix: change server url to localhost

--- client/src/socket/index.ts
+++ client/src/socket/index.ts
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SERVER = 'https://chat-app-backend-eight.vercel.app/'
+// const SERVER = 'https://chat-app-backend-eight.vercel.app/'
+const SERVER = 'http://localhost:3001'
 
-export const socket = io(SERVER, { transports : ['websocket'] })
+export const socket = io(SERVER, { transports: ['websocket'] })
