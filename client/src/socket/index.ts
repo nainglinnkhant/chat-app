c6379ad48ab28db06d645402eacc8e1c57ef3575
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-05-06 17:44:39 +0630

    feat: use hosted server in prod env

--- client/src/socket/index.ts
+++ client/src/socket/index.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client'
 
-// const SERVER = 'https://chat-app-backend-eight.vercel.app/'
-const SERVER = 'http://localhost:3001'
+const SERVER =
+  process.env.NODE_ENV === 'production'
+    ? 'https://chat-app-108c.onrender.com/'
+    : 'http://localhost:3001'
 
 export const socket = io(SERVER, { transports: ['websocket'] })
