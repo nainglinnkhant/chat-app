1d6c4c0a86df4c45607690e8cc72ca6642f11885
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-06-09 00:15:16 +0630

    fix: change backend host to Railway
    Switch backend host from Render to Railway because Render had a very long cold start

--- client/src/socket/index.ts
+++ client/src/socket/index.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 const SERVER =
   process.env.NODE_ENV === 'production'
-    ? 'https://chat-app-108c.onrender.com/'
+    ? 'chat-app-production-9bf6.up.railway.app'
     : 'http://localhost:3001'
 
 export const socket = io(SERVER, { transports: ['websocket'] })
