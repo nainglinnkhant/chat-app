12cd0d71734c73fb07565a8ff6b463220bf7d4e2
nainglinnkhant <nainglinnkhnt@gmail.com>
2023-07-23 03:02:41 +0630

    fix: fix messageTypes not found

--- server/src/data/messages.ts
+++ server/src/data/messages.ts
@@ -1,13 +1,11 @@
 import type { Message, Messages } from '../types'
 
-const { TEXT, IMAGE } = require('../constants/messageTypes')
-
 const messages: Messages = {}
 
 const getRoomMessages = (roomName: string, userId: string) => {
   return messages[roomName]?.filter(message => {
     // All messages with type 'text' and 'image' are returned
-    if (message.type === TEXT || message.type === IMAGE) return true
+    if (message.type === 'text' || message.type === 'image') return true
     // Only 'notification' messages that does not match with userId are returned
     return message.sender?.id !== userId
   })
